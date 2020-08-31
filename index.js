5ba93d37036b5dbc2549e3561e4ec1777ea8de82
temorymatine <temorymatine@gmail.com>
2020-08-31 01:25:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }})
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      let leftNumbers = dodger.style.right.replace("px", "");
+      let left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left= `${left + 1}px`;
+    }
+  });
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
